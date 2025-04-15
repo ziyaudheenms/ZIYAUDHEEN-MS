14173be3b04d75e8111df43450b33c4d7753a777
Ziyaudheen <shajeem229@gmail.com>
2025-04-15 16:09:32 +0530

    seventh commit

--- elements/Aim.jsx
+++ elements/Aim.jsx
@@ -9,7 +9,7 @@ import {
 
 export function Aim({title,disc}) {
   return (
-    <div className="flex py-2  items-center justify-center antialiased">
+    <div className="flex py-2 w-full  items-center justify-center antialiased">
       <GlowingStarsBackgroundCard>
         <GlowingStarsTitle>{title}</GlowingStarsTitle>
         <div className="flex justify-between items-end">
